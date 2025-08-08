fc1ecf735620a2d926dce6d534fc5a1854421119
numerimondes <165125632+emyassine@users.noreply.github.com>
2025-08-08 12:18:24 +0100

    Assets Update

--- assets/print_dir.sh
+++ assets/print_dir.sh
@@ -6,13 +6,13 @@ GITHUB_REPO=".github"
 BRANCH="main"
 BASE_URL="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO/refs/heads/$BRANCH"
 
-# Output file path
+# Output file path (relative to current `assets/` directory)
 OUTPUT_FILE="commons/config/assets-links.txt"
 
 # Create parent directory if it doesn't exist
 mkdir -p "$(dirname "$OUTPUT_FILE")"
 
-# Clear the output file if it exists
+# Clear the output file
 > "$OUTPUT_FILE"
 
 # Find and process files
@@ -20,6 +20,7 @@ find . -type f \
     ! -name "*.txt" \
     ! -name "*.sh" \
     ! -name ".gitkeep" \
+    ! -name "txt.gitkeep" \
     | while read -r file; do
         clean_path="${file#./}"
         echo "$BASE_URL/assets/$clean_path" >> "$OUTPUT_FILE"
